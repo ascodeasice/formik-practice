9b454e074a137c549a47ba8513822286c24b316b
Leo Wang <ab0988956087@gamil.com>
2023-06-25 20:11:23 +0800

    feat(app.tsx): use formik to control value of input

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,35 @@ import { TextField,Stack, Container, Typography} from '@mui/material';
 import { ThemeProvider} from '@mui/material/styles';
 import theme from './theme';
 import './App.css'
+import { useFormik } from "formik";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      email: ""
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container sx={{ bgcolor:theme.palette.primary.light, minHeight:'100vh',minWidth:'100vw',display:'flex',justifyContent:'center'}}>
-        <Stack spacing={2} mt={8}>
-        <Typography variant='h1' color={'white'}>Sign up now</Typography>
-        <TextField
-          id="email"
-          label="Email"
-          value={''}
-          />
-          </Stack>
-    </Container>
-        </ThemeProvider>
+    <form  onSubmit={formik.handleSubmit}>
+      <ThemeProvider theme={theme}>
+        <Container sx={{ bgcolor:theme.palette.primary.light, minHeight:'100vh',minWidth:'100vw',display:'flex',justifyContent:'center'}}>
+          <Stack spacing={2} mt={8}>
+          <Typography variant='h1' color={'white'}>Sign up now</Typography>
+          <TextField
+            id="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            type='email'
+            />
+            </Stack>
+        </Container>
+      </ThemeProvider>
+    </form>
     )
 }
 
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
-import { render } from "@testing-library/react";
+import { render,fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
+
 
 test("測試 App.tsx 頁面是否正常運作", async () => {
   render(<App />);
@@ -28,4 +29,19 @@ test('renders heading with text: Sign up now', () => {
 
   // Assert that the heading element is present
   expect(headingElement).toBeInTheDocument();
+});
+
+test('typing into input with name "Email" updates its value', () => {
+  // Render the App component
+  const { getByLabelText } = render(<App />);
+
+  // Select the input element by its associated label text
+  const emailInput = getByLabelText('Email');
+
+  // Simulate typing into the input field
+  const typedText = 'test@example.com';
+  fireEvent.change(emailInput, { target: { value: typedText } });
+
+  // Assert that the input value has been updated correctly
+  expect(emailInput).toHaveValue(typedText);
 });
